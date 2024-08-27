b42cadc7adf6a2010a6d1f850e910ea60734a845
Enotove <enotove@gmail.com>
2024-08-27 17:54:50 +0500

    Init commit

---
+++ Ptr_Lesson_02.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+
+class node
+{
+public:
+    int m_value;
+    
+    node(int value) : m_value{ value } {};
+    ~node() { std::cout << "destructor called\n"; }
+    std::weak_ptr<node> parent;
+    
+};
+
+int main()
+{
+    
+        auto node1 = std::make_shared<node>(1);
+        auto node2 = std::make_shared<node>(2);
+       
+        node1->parent = node2;
+        node2->parent = node1;
+    
+
+    return 0;
+}
